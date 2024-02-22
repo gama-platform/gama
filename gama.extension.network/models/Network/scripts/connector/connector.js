d1e3cdb3276d47eb3654f3b2e5da58b6b22991e2
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2024-02-22 19:21:15 +0700

    Improves gama connector
    - adds help page
    - change the layout to columns
    - change the stacking direction of messages
    - general graphical improvements

--- gama.extension.network/models/Network/scripts/connector/connector.js
+++ gama.extension.network/models/Network/scripts/connector/connector.js
@@ -15,6 +15,10 @@ const NOTREADY = "NOTREADY"
 const PAUSED = "PAUSED"
 const RUNNING = "RUNNING"
 
+//Request or response type
+const REQUEST = "REQUEST"
+const RESPONSE = "RESPONSE"
+
 // Global variables
 var gama_ws_port = DEFAULT_GAMA_WS_PORT
 var gama_ip_address = DEFAULT_GAMA_IP_ADDRESS
@@ -88,7 +92,7 @@ function send_step() {
         "type": "step",
         "exp_id": experiment_id,
         "nb_step": current_nb_step
-      }
+    }
 }
 
 function send_step_back() {
@@ -96,7 +100,7 @@ function send_step_back() {
         "type": "stepBack",
         "exp_id": experiment_id,
         "nb_step": current_nb_step_back
-      }
+    }
 }
 
 // Setters
@@ -113,7 +117,7 @@ function setGamaState(newState) {
     gama_state = newState
     state_html = document.querySelector("#gama-state")
     switch (gama_state) {
-        case UNKNOWN: state_html.innerHTML = "?"; state_html.style = "color: white;"; break;
+        case UNKNOWN: state_html.innerHTML = "UNKNOWN"; state_html.style = "color: white;"; break;
         case NONE: state_html.innerHTML = "NONE (no experiment running)"; state_html.style = "color: red;"; break;
         case NOTREADY: state_html.innerHTML = "NOTREADY (the experiment is not ready)"; state_html.style = "color: red;"; break;
         case PAUSED: state_html.innerHTML = "PAUSED (the experiment is paused)"; state_html.style = "color: orange;"; break;
@@ -148,10 +152,10 @@ function createWebSocketClient() {
                     break;
                 default: break;
             }
-            logResponse(message)
+            logRequestResponse(message, RESPONSE)
         }
         catch (exception) {
-            logResponse("An error occured when parsing the last message from Gama Server")
+            logRequestResponse("An error occured when parsing the last message from Gama Server", RESPONSE)
         }
     }
 
@@ -174,119 +178,119 @@ function createWebSocketClient() {
 
 function load() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([NOTREADY, PAUSED, RUNNING].includes(gama_state) && model_file != undefined && experiment_name != undefined) {
         socket.send(JSON.stringify(reload_experiment()))
-        logRequest(reload_experiment())
+        logRequestResponse(reload_experiment(), REQUEST)
         return
     }
     if ([UNKNOWN, NONE].includes(gama_state) && model_file != undefined && experiment_name != undefined) {
         socket.send(JSON.stringify(load_experiment()))
-        logRequest(load_experiment())
+        logRequestResponse(load_experiment(), REQUEST)
         return
     }
     else {
-        logRequest("Could not send Load or Reload since the model file or the experiment name is undefined")
+        logRequestResponse("Could not send Load or Reload since the model file or the experiment name is undefined", REQUEST)
     }
 }
 
 function start() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(play_experiment()))
-        logRequest(play_experiment())
+        logRequestResponse(play_experiment(), REQUEST)
     }
     else {
-        logRequest("Could not send Play since Gama Server is not currently loaded or paused")
+        logRequestResponse("Could not send Play since Gama Server is not currently loaded nor paused", REQUEST)
     }
 }
 
 function pause() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([RUNNING].includes(gama_state)) {
         socket.send(JSON.stringify(pause_experiment()))
-        logRequest(pause_experiment())
+        logRequestResponse(pause_experiment(), REQUEST)
     }
     else {
-        logRequest("Could not send Pause since Gama Server is not currently running")
+        logRequestResponse("Could not send Pause since Gama Server is not currently running",REQUEST)
     }
 }
 
 function end() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected",REQUEST)
         return
     }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(stop_experiment()))
-        logRequest(stop_experiment())
+        logRequestResponse(stop_experiment(),REQUEST)
     }
     else {
-        logRequest("Could not send Stop since Gama Server is not currently running")
+        logRequestResponse("Could not send Stop since Gama Server is not currently running",REQUEST)
     }
 }
 
 function expression() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(send_expression()))
-        logRequest(send_expression())
+        logRequestResponse(send_expression(), REQUEST)
     }
     else {
-        logRequest("Could not send Expression since Gama Server is not currently running")
+        logRequestResponse("Could not send Expression since Gama Server is not currently running",REQUEST)
     }
 }
 
 function ask() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(send_ask()))
-        logRequest(send_ask())
+        logRequestResponse(send_ask(), REQUEST)
     }
     else {
-        logRequest("Could not send Ask since Gama Server is not currently running")
+        logRequestResponse("Could not send Ask since Gama Server is not currently running", REQUEST)
     }
 }
 
 function step() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(send_step()))
-        logRequest(send_step())
+        logRequestResponse(send_step(), REQUEST)
     }
     else {
-        logRequest("Could not send Step since Gama Server is not currently running")
+        logRequestResponse("Could not send Step since Gama Server is not currently running", REQUEST)
     }
 }
 
 function stepBack() {
     if (connection_state == DISCONNECTED) {
-        logRequest("Gama Server is not connected")
+        logRequestResponse("Gama Server is not connected", REQUEST)
         return
     }
     if ([RUNNING, PAUSED].includes(gama_state)) {
         socket.send(JSON.stringify(send_step_back()))
-        logRequest(send_step_back())
+        logRequestResponse(send_step_back(), REQUEST)
     }
     else {
-        logRequest("Could not send Step Back since Gama Server is not currently running")
+        logRequestResponse("Could not send Step Back since Gama Server is not currently running", REQUEST)
     }
 }
 
@@ -334,55 +338,48 @@ function restartConnector() {
     socket = createWebSocketClient()
 }
 
-function logRequest(message) {
+function logRequestResponse(message, type) {
     var title_message
     var content_message
-    const request_div = document.createElement('div')
-    document.querySelector("#request-response-timeline").appendChild(request_div)
-    request_div.classList.add("request")
+    const reqres_div = document.createElement('div')
+    const parent = document.querySelector("#request-response-timeline")
+    parent.appendChild(reqres_div)
+    parent.insertBefore(reqres_div, parent.firstChild);
+    if (type==REQUEST) reqres_div.classList.add("request")
+    else if (type==RESPONSE) reqres_div.classList.add("response")
     if (message.type != undefined) {
-        title_message = "Sending message of type "+message.type
+        var message_type = message.type
+        switch (message_type) {
+            case 'load': message_type = writeColored(message.type, 'lightblue'); break;
+            case 'reload': message_type = writeColored(message.type, 'lightblue'); break;
+            case 'play': message_type = writeColored(message.type, 'green'); break;
+            case 'pause': message_type = writeColored(message.type, 'orange'); break;
+            case 'stop': message_type = writeColored(message.type, 'red'); break;
+            case 'CommandExecutedSuccessfully': message_type = writeColored(message.type, 'green'); break;
+            case 'SimulationStatus': message_type = writeColored(message.type, 'lightblue'); break;
+            case 'UnableToExecuteRequest': message_type = writeColored(message.type, 'red'); break;
+            case 'GamaServerError': message_type = writeColored(message.type, 'red'); break;
+            default : break;
+        }
+        if (type == REQUEST) title_message = "Sending message of type "+ message_type
+        else if (type==RESPONSE) title_message = "Receiving message of type "+ message_type
         const title_div = document.createElement('div')
-        request_div.appendChild(title_div)
-        request_div
+        reqres_div.appendChild(title_div)
         title_div.innerHTML = title_message
         content_message = '<pre>' + JSON.stringify(message, null, 2) + '</pre>'
         const content_div = document.createElement('div')
-        request_div.appendChild(content_div)
+        reqres_div.appendChild(content_div)
         content_div.innerHTML = content_message
     }
     else {
         title_message = message
         const title_div = document.createElement('div')
-        request_div.appendChild(title_div)
+        reqres_div.appendChild(title_div)
         title_div.innerHTML = title_message
         title_div.style = "color:red;"
     }
 }
 
-function logResponse(message) {
-    var title_message
-    var content_message
-    const response_div = document.createElement('div')
-    document.querySelector("#request-response-timeline").appendChild(response_div)
-    response_div.classList.add("response")
-    console.log(message);
-    if (message.type != undefined) {
-        title_message = "Receiving message of type "+ message.type
-        const title_div = document.createElement('div')
-        response_div.appendChild(title_div)
-        title_div.innerHTML = title_message
-        content_message = '<pre><code>' + JSON.stringify(message, null, 2) + '</code></pre>'
-        const content_div = document.createElement('div')
-        response_div.appendChild(content_div)
-        content_div.innerHTML = content_message
-    }
-    else {
-        title_message = message
-        const title_div = document.createElement('div')
-        response_div.appendChild(title_div)
-        title_div.innerHTML = title_message
-        title_div.style = "color:red;"
-    }
+function writeColored(msg, color) {
+    return '<span style="color:' + color + ';">'+ msg + '</span>'
 }
-
