0a353727f9a1f9ddf06a62b83dd2b39db2bfe841
AlexisDrogoul <alexis.drogoul@gmail.com>
2024-03-19 21:50:35 +0700

    Fixes #128 - The color parameter was forgotten in the console listener

--- gama.core/src/gama/core/common/CompositeConsoleListener.java
+++ gama.core/src/gama/core/common/CompositeConsoleListener.java
@@ -1,12 +1,12 @@
 /*******************************************************************************************************
  *
- * CompositeConsoleListener.java, in gama.core, is part of the source code of the GAMA modeling and simulation
- * platform .
+ * CompositeConsoleListener.java, in gama.core, is part of the source code of the
+ * GAMA modeling and simulation platform (v.2024-06).
  *
- * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- *
+ * 
  ********************************************************************************************************/
 package gama.core.common;
 
@@ -42,7 +42,7 @@ public class CompositeConsoleListener implements IConsoleListener {
 
 	@Override
 	public void informConsole(final String s, final ITopLevelAgent root, final GamaColor color) {
-		for (IConsoleListener console : consoles) { console.informConsole(s, root); }
+		for (IConsoleListener console : consoles) { console.informConsole(s, root, color); }
 	}
 
 	/**
