e413fb2dcb3215badb029e77b992216937be0d97
AlexisDrogoul <alexis.drogoul@gmail.com>
2025-01-04 18:50:15 +0700

    Fixes #388
    Fixes #388 by stating which types are being incorrectly compared.

--- gama.core/src/gama/gaml/operators/Comparison.java
+++ gama.core/src/gama/gaml/operators/Comparison.java
@@ -1,28 +1,26 @@
 /*******************************************************************************************************
  *
- * Comparison.java, in gama.core, is part of the source code of the
- * GAMA modeling and simulation platform .
+ * Comparison.java, in gama.core, is part of the source code of the GAMA modeling and simulation platform .
  *
  * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- * 
+ *
  ********************************************************************************************************/
 package gama.gaml.operators;
 
 import static org.locationtech.jts.index.quadtree.IntervalSize.isZeroWidth;
 
 import org.eclipse.emf.ecore.EObject;
-
 import org.locationtech.jts.index.quadtree.IntervalSize;
 
-import gama.annotations.precompiler.IConcept;
-import gama.annotations.precompiler.IOperatorCategory;
 import gama.annotations.precompiler.GamlAnnotations.doc;
 import gama.annotations.precompiler.GamlAnnotations.example;
 import gama.annotations.precompiler.GamlAnnotations.operator;
 import gama.annotations.precompiler.GamlAnnotations.test;
 import gama.annotations.precompiler.GamlAnnotations.usage;
+import gama.annotations.precompiler.IConcept;
+import gama.annotations.precompiler.IOperatorCategory;
 import gama.core.metamodel.shape.GamaPoint;
 import gama.gaml.compilation.IOperatorValidator;
 import gama.gaml.compilation.annotations.validator;
@@ -49,9 +47,10 @@ public class Comparison {
 			if (arguments.length > 1) {
 				final IType<?> t1 = arguments[0].getGamlType();
 				final IType<?> t2 = arguments[1].getGamlType();
-				if (t1.id() == IType.NONE || t2.id() == IType.NONE) { return true; }
-				if (t1.isTranslatableInto(t2) || t2.isTranslatableInto(t1)) { return true; }
-				context.warning("This equality will always return false", IGamlIssue.UNMATCHED_OPERANDS, emfContext);
+				if (t1.id() == IType.NONE || t2.id() == IType.NONE || t1.isTranslatableInto(t2) || t2.isTranslatableInto(t1)) return true;
+				context.warning(
+						"This equality will always return false because you are comparing a " + t1 + " with a " + t2,
+						IGamlIssue.UNMATCHED_OPERANDS, emfContext);
 			}
 			return true;
 		}
@@ -60,22 +59,25 @@ public class Comparison {
 
 	/** The Constant GT. */
 	public final static String GT = ">";
-	
+
 	/** The Constant LT. */
 	public final static String LT = "<";
-	
+
 	/** The Constant GTE. */
 	public final static String GTE = ">=";
-	
+
 	/** The Constant LTE. */
 	public final static String LTE = "<=";
 
 	/**
 	 * Between.
 	 *
-	 * @param a the a
-	 * @param inf the inf
-	 * @param sup the sup
+	 * @param a
+	 *            the a
+	 * @param inf
+	 *            the inf
+	 * @param sup
+	 *            the sup
 	 * @return the boolean
 	 */
 	@operator (
@@ -94,16 +96,19 @@ public class Comparison {
 	@test ("not(1 between(1,4))")
 	@test ("not(2 between(4,1))")
 	public static Boolean between(final Integer a, final Integer inf, final Integer sup) {
-		if (inf > sup) { return false; }
+		if (inf > sup) return false;
 		return a >= sup ? false : a > inf;
 	}
 
 	/**
 	 * Between.
 	 *
-	 * @param a the a
-	 * @param inf the inf
-	 * @param sup the sup
+	 * @param a
+	 *            the a
+	 * @param inf
+	 *            the inf
+	 * @param sup
+	 *            the sup
 	 * @return the boolean
 	 */
 	@operator (
@@ -121,15 +126,17 @@ public class Comparison {
 	@test ("not(1.0 between(1.0,4.0))")
 	@test ("not(2.2 between(4.0,1.9))")
 	public static Boolean between(final Double a, final Double inf, final Double sup) {
-		if (inf > sup) { return false; }
+		if (inf > sup) return false;
 		return a >= sup ? false : a > inf;
 	}
 
 	/**
 	 * Greater.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -150,15 +157,17 @@ public class Comparison {
 	@test (
 			value = "val <- (13 > 7); val = true")
 	public static Boolean greater(final Integer a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a > b;
 	}
 
 	/**
 	 * Greater.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -172,15 +181,17 @@ public class Comparison {
 					value = "3 > 2.5",
 					equals = "true") })
 	public static Boolean greater(final Integer a, final Double b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a > b;
 	}
 
 	/**
 	 * Greater.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -194,15 +205,17 @@ public class Comparison {
 					value = "3.5 > 7",
 					equals = "false") })
 	public static Boolean greater(final Double a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a > b;
 	}
 
 	/**
 	 * Greater.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -216,15 +229,17 @@ public class Comparison {
 					value = "3.5 > 7.6",
 					equals = "false") })
 	public static Boolean greater(final Double a, final Double b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a > b;
 	}
 
 	/**
 	 * Less.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -241,15 +256,17 @@ public class Comparison {
 					equals = "true") },
 			see = { GT, GTE, LTE, "=", "!=" })
 	public static Boolean less(final Integer a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a < b;
 	}
 
 	/**
 	 * Less.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -263,15 +280,17 @@ public class Comparison {
 					value = "3 < 2.5",
 					equals = "false") })
 	public static Boolean less(final Integer a, final Double b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a < b;
 	}
 
 	/**
 	 * Less.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -285,15 +304,17 @@ public class Comparison {
 					value = "3.5 < 7",
 					equals = "true") })
 	public static Boolean less(final Double a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a < b;
 	}
 
 	/**
 	 * Less.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -307,15 +328,17 @@ public class Comparison {
 					value = "3.5 < 7.6",
 					equals = "true") })
 	public static Boolean less(final Double a, final Double b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a < b;
 	}
 
 	/**
 	 * Greater or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -332,15 +355,17 @@ public class Comparison {
 					equals = "false") },
 			see = { GT, LT, LTE, "=", "!=" })
 	public static Boolean greaterOrEqual(final Integer a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a >= b;
 	}
 
 	/**
 	 * Greater or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -354,15 +379,17 @@ public class Comparison {
 					value = "3 >= 2.5",
 					equals = "true") })
 	public static Boolean greaterOrEqual(final Integer a, final Double b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a >= b;
 	}
 
 	/**
 	 * Greater or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -376,15 +403,17 @@ public class Comparison {
 					value = "3.5 >= 7",
 					equals = "false") })
 	public static Boolean greaterOrEqual(final Double a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a >= b;
 	}
 
 	/**
 	 * Greater or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -398,15 +427,17 @@ public class Comparison {
 					value = "3.5 >= 3.5",
 					equals = "true") })
 	public static Boolean greaterOrEqual(final Double a, final Double b) {
-		if (a == null || b == null) { return false; }
-		return !(a < b);
+		if (a == null || b == null) return false;
+		return (a >= b);
 	}
 
 	/**
 	 * Op less than or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -422,15 +453,17 @@ public class Comparison {
 					value = "3 <= 7",
 					equals = "true") })
 	public static Boolean opLessThanOrEqual(final Integer a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a <= b;
 	}
 
 	/**
 	 * Less or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -445,15 +478,17 @@ public class Comparison {
 					equals = "false") },
 			see = { GT, LT, GTE, "=", "!=" })
 	public static Boolean lessOrEqual(final Integer a, final Double b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a <= b;
 	}
 
 	/**
 	 * Less or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -467,15 +502,17 @@ public class Comparison {
 					value = "7.0 <= 7",
 					equals = "true") })
 	public static Boolean lessOrEqual(final Double a, final Integer b) {
-		if (a == null || b == null) { return false; }
+		if (a == null || b == null) return false;
 		return a <= b;
 	}
 
 	/**
 	 * Less or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -489,15 +526,17 @@ public class Comparison {
 					value = "3.5 <= 3.5",
 					equals = "true") })
 	public static Boolean lessOrEqual(final Double a, final Double b) {
-		if (a == null || b == null) { return false; }
-		return !(a > b);
+		if (a == null || b == null) return false;
+		return (a <= b);
 	}
 
 	/**
 	 * Equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -520,8 +559,10 @@ public class Comparison {
 	/**
 	 * Equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -544,8 +585,10 @@ public class Comparison {
 	/**
 	 * Equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -570,8 +613,10 @@ public class Comparison {
 	/**
 	 * Equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -593,8 +638,10 @@ public class Comparison {
 	/**
 	 * Different.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -613,8 +660,8 @@ public class Comparison {
 							equals = "true") },
 			see = { "=", GT, LT, GTE, LTE, "=" })
 	public static Boolean different(final Double a, final Double b) {
-		if (a == null) { return b != null; }
-		if (b == null) { return false; }
+		if (a == null) return b != null;
+		if (b == null) return false;
 		return !IntervalSize.isZeroWidth(a, b);
 		// return a < b || a > b;
 	}
@@ -622,8 +669,10 @@ public class Comparison {
 	/**
 	 * Different.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -648,8 +697,10 @@ public class Comparison {
 	/**
 	 * Different.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -673,8 +724,10 @@ public class Comparison {
 	/**
 	 * Less or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -690,7 +743,7 @@ public class Comparison {
 							value = "'abc' <= 'aeb'",
 							equals = "true") }))
 	public static Boolean lessOrEqual(final String a, final String b) {
-		if (a == null) { return false; }
+		if (a == null) return false;
 		final int i = a.compareTo(b);
 		return i <= 0;
 	}
@@ -698,8 +751,10 @@ public class Comparison {
 	/**
 	 * Greater or equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -718,7 +773,7 @@ public class Comparison {
 									value = "'abc' >= 'abc'",
 									equals = "true") }))
 	public static Boolean greaterOrEqual(final String a, final String b) {
-		if (a == null) { return false; }
+		if (a == null) return false;
 		final int i = a.compareTo(b);
 		return i >= 0;
 	}
@@ -726,8 +781,10 @@ public class Comparison {
 	/**
 	 * Less.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -743,7 +800,7 @@ public class Comparison {
 							value = "'abc' < 'aeb'",
 							equals = "true") }))
 	public static Boolean less(final String a, final String b) {
-		if (a == null) { return false; }
+		if (a == null) return false;
 		final int i = a.compareTo(b);
 		return i < 0;
 	}
@@ -751,8 +808,10 @@ public class Comparison {
 	/**
 	 * Greater.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -768,7 +827,7 @@ public class Comparison {
 							value = "'abc' > 'aeb'",
 							equals = "false") }))
 	public static Boolean greater(final String a, final String b) {
-		if (a == null) { return false; }
+		if (a == null) return false;
 		final int i = a.compareTo(b);
 		return i > 0;
 	}
@@ -776,8 +835,10 @@ public class Comparison {
 	/**
 	 * Equal.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -799,8 +860,10 @@ public class Comparison {
 	/**
 	 * Different.
 	 *
-	 * @param a the a
-	 * @param b the b
+	 * @param a
+	 *            the a
+	 * @param b
+	 *            the b
 	 * @return the boolean
 	 */
 	@operator (
@@ -823,8 +886,10 @@ public class Comparison {
 	/**
 	 * Less.
 	 *
-	 * @param p1 the p 1
-	 * @param p the p
+	 * @param p1
+	 *            the p 1
+	 * @param p
+	 *            the p
 	 * @return the boolean
 	 */
 	@operator (
@@ -849,8 +914,10 @@ public class Comparison {
 	/**
 	 * Greater.
 	 *
-	 * @param p1 the p 1
-	 * @param p the p
+	 * @param p1
+	 *            the p 1
+	 * @param p
+	 *            the p
 	 * @return the boolean
 	 */
 	@operator (
@@ -875,8 +942,10 @@ public class Comparison {
 	/**
 	 * Less or equal.
 	 *
-	 * @param p1 the p 1
-	 * @param p the p
+	 * @param p1
+	 *            the p 1
+	 * @param p
+	 *            the p
 	 * @return the boolean
 	 */
 	@operator (
@@ -901,8 +970,10 @@ public class Comparison {
 	/**
 	 * Greater or equal.
 	 *
-	 * @param p1 the p 1
-	 * @param p the p
+	 * @param p1
+	 *            the p 1
+	 * @param p
+	 *            the p
 	 * @return the boolean
 	 */
 	@operator (
