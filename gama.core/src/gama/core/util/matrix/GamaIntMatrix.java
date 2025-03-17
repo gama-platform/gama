930123fc516569562e2ca9110f0af378e33cd80e
AlexisDrogoul <alexis.drogoul@gmail.com>
2025-03-17 23:12:05 +0700

    Removes hashCode computation from matrices
    No real reason these hashCode computations were useful.

--- gama.core/src/gama/core/util/matrix/GamaIntMatrix.java
+++ gama.core/src/gama/core/util/matrix/GamaIntMatrix.java
@@ -1,9 +1,9 @@
 /*******************************************************************************************************
  *
  * GamaIntMatrix.java, in gama.core, is part of the source code of the GAMA modeling and simulation platform
- * .
+ * (v.2025-03).
  *
- * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2025 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -327,10 +327,12 @@ public class GamaIntMatrix extends GamaMatrix<Integer> implements IImageProvider
 		return Arrays.equals(this.matrix, mat.matrix);
 	}
 
-	@Override
-	public int hashCode() {
-		return Arrays.hashCode(matrix);
-	}
+	// Removed to improve performances
+	//
+	// @Override
+	// public int hashCode() {
+	// return Arrays.hashCode(matrix);
+	// }
 
 	/**
 	 * Fill with.
