5281705e76bbf0e377eade7774af6458c5a38ab8
AlexisDrogoul <alexis.drogoul@gmail.com>
2025-03-25 13:40:35 +0700

    Adds a better test to track invocations w/o arguments
    Fixes #447

--- gama.core/src/gama/gaml/descriptions/IDescription.java
+++ gama.core/src/gama/gaml/descriptions/IDescription.java
@@ -1,8 +1,8 @@
 /*******************************************************************************************************
  *
- * IDescription.java, in gama.core, is part of the source code of the GAMA modeling and simulation platform .
+ * IDescription.java, in gama.core, is part of the source code of the GAMA modeling and simulation platform (v.2025-03).
  *
- * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2025 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -109,7 +109,11 @@ public interface IDescription extends IGamlDescription, IKeyword, ITyped, IDispo
 
 		/** The is memorize. */
 		isMemorize,
+
+		/** The is batch. */
 		isBatch,
+		/** The Is create. */
+		IsCreate,
 
 	}
 
