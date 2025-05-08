ff2151276adf6f02a2c7cd12e18bf19c6edd3f70
AlexisDrogoul <alexis.drogoul@gmail.com>
2025-05-08 18:55:24 +0700

    Update SimulationPerspective.java
    Fixes #613

--- gama.ui.experiment/src/gama/ui/experiment/perspective/SimulationPerspective.java
+++ gama.ui.experiment/src/gama/ui/experiment/perspective/SimulationPerspective.java
@@ -1,12 +1,12 @@
 /*******************************************************************************************************
  *
- * SimulationPerspective.java, in gama.ui.shared.experiment, is part of the source code of the
- * GAMA modeling and simulation platform .
+ * SimulationPerspective.java, in gama.ui.experiment, is part of the source code of the GAMA modeling and simulation
+ * platform (v.2025-03).
  *
- * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2025 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- * 
+ *
  ********************************************************************************************************/
 package gama.ui.experiment.perspective;
 
@@ -37,8 +37,8 @@ public class SimulationPerspective implements IPerspectiveFactory {
 
 		final IFolderLayout consoleFolder = layout.createFolder("consoles", IPageLayout.BOTTOM, 0.70f, "navigAndParam");
 
-		consoleFolder.addView(IGui.INTERACTIVE_CONSOLE_VIEW_ID);
 		consoleFolder.addView(IGui.CONSOLE_VIEW_ID);
+		consoleFolder.addView(IGui.INTERACTIVE_CONSOLE_VIEW_ID);
 
 		final IPlaceholderFolderLayout displays =
 				layout.createPlaceholderFolder("displays", IPageLayout.TOP, 0.7f, editor);
