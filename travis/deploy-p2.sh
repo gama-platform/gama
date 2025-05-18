1c29595460fb4c2122ee9fe207fa9504b82176a6
RoiArthurB <contact@arthurbrugiere.fr>
2025-05-18 15:43:18 +0700

    [p2] Align travis scripts to new cleaned execution + Add documentation in the parent's pom.xml for not loosing track of it

--- travis/deploy-p2.sh
+++ travis/deploy-p2.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "Publishing module gama.annotations on p2 site"
-cd $( dirname $( realpath "${BASH_SOURCE[0]}" ) )/../gama.annotations
-mvn deploy --settings ../travis/settings.xml -DskipTests=true -B "$@"
-
-echo "Publishing module gama.processor on p2 site"
-cd ../gama.processor 
-mvn deploy --settings ../travis/settings.xml -DskipTests=true -B "$@"
-
-echo "Publishing module gama.parent on p2 site"
-cd ../gama.parent 
-mvn deploy --settings ../travis/settings.xml -DskipTests=true -B "$@"
+echo "Publishing p2 site public OVH server"
+cd $( dirname $( realpath "${BASH_SOURCE[0]}" ) )/../gama.p2site
+mvn clean install --settings ../travis/settings.xml -DskipTests=true -B "$@"
