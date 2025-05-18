de6a1d89c36a011b9f725bedadeeb3fb9f070e30
RoiArthurB <contact@arthurbrugiere.fr>
2025-05-18 19:24:48 +0700

    Fix gama release compilation Fix wrappers not working because of JVM needs, bad regex swap, etc

--- gama.product/extraresources/headless/unix/gama-headless.sh
+++ gama.product/extraresources/headless/unix/gama-headless.sh
@@ -20,8 +20,8 @@ if [ -d "${headlessPath}/../jdk" ]; then
 else
   javaVersion=$(java -version 2>&1 | head -n 1 | cut -d "\"" -f 2)
   # Check if good Java version before everything
-  if [[ ${javaVersion:2} == 23 ]]; then
-    echo "You should use Java 23 to run GAMA"
+  if [[ ${javaVersion:2} == 21 ]]; then
+    echo "You should use Java 21 to run GAMA"
     echo "Found you using version : $javaVersion"
     exit 1
   fi
