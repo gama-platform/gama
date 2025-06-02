dd69112ad9e4e064b64a4116b4f009052dad4526
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2025-06-02 14:08:49 +0700

    [GS] adds URI to compilation error outputs

--- gama.core/src/gama/gaml/compilation/GamaCompilationFailedException.java
+++ gama.core/src/gama/gaml/compilation/GamaCompilationFailedException.java
@@ -36,7 +36,8 @@ public class GamaCompilationFailedException extends RuntimeException{
 			sb.append("\"message\":\"").append(error.message).append("\",");
 			sb.append("\"code\":\"").append(error.code).append("\",");
 			sb.append("\"data\":[\"").append(String.join("\",\"", error.data != null ? error.data : new String[0])).append("\"],");
-			sb.append("\"source\":\"").append(error.source != null ? FileUtils.escapeFilePath(error.source.eResource().getURI().toFileString()) : "" ).append("\"");	
+			sb.append("\"source\":\"").append(error.source != null ? FileUtils.escapeFilePath(error.source.eResource().getURI().toFileString()) : "" ).append("\",");	
+			sb.append("\"uri\":\"").append(error.getURI()).append("\"");
 			sb.append("}");
 			atLeastSecond = true;
 		}
