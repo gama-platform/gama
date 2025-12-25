c47574aeb1bcada37b4477191466237e1012a0be
AlexisDrogoul <alexis.drogoul@gmail.com>
2025-12-25 23:00:17 +0700

    Refactor Bullet library structure and update Java version
    Moved Bullet JME libraries and native files from 'lib/' to 'libs/Libbulletjme 21.2.1/' to improve organization. Updated project configuration and paths to reflect the new structure. Set Java compiler compliance and execution environment to Java 21. Updated copyright and version info.

--- gama.extension.physics/src/gama/extension/physics/NativeLoader.java
+++ gama.extension.physics/src/gama/extension/physics/NativeLoader.java
@@ -1,9 +1,9 @@
 /*******************************************************************************************************
  *
- * NativeLoader.java, in gaml.extensions.physics, is part of the source code of the GAMA modeling and
- * simulation platform .
+ * NativeLoader.java, in gama.extension.physics, is part of the source code of the GAMA modeling and simulation platform
+ * (v.2025-03).
  *
- * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2025 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -34,7 +34,7 @@ public class NativeLoader {
 	public static Boolean NATIVE_BULLET_LIBRARY_LOADED = null;
 
 	/** The Constant NATIVE_LIBRARY_LOCATION. */
-	public static final String NATIVE_LIBRARY_LOCATION = "/lib/native/";
+	public static final String NATIVE_LIBRARY_LOCATION = "/libs/Libbulletjme 21.2.1/native/";
 
 	/** The Constant MAC_NATIVE_LIBRARY_NAME. */
 	public static final String MAC_NATIVE_LIBRARY_NAME = "MacOSX64ReleaseDp_libbulletjme.dylib";
