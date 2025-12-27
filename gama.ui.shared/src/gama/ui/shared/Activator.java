8d5a2fd5cb70eb64e2a40856357e9cbcc22ae9f8
AlexisDrogoul <alexis.drogoul@gmail.com>
2025-12-28 00:19:56 +0700

    Enhance icon substitution and support disabled icons
    Expanded the documentation for dynamic icon substitutions and added mappings for additional icons, including their disabled states. Refactored the replace method to handle disabled icons, ensuring both enabled and disabled icon variants are registered in the image registry.

--- gama.ui.shared/src/gama/ui/shared/Activator.java
+++ gama.ui.shared/src/gama/ui/shared/Activator.java
@@ -56,7 +56,65 @@ public class Activator extends AbstractUIPlugin {
 	public static final class GamaResourceUtilities extends ResourceUtility {
 
 		/**
-		 * The dynamic substitions. Not used for the moment. Kept for reference
+		 * The dynamic substitions. Not used for the moment. Kept for reference in order to implement substitutions with
+		 * GamaIcons if needed. The current implementation lists only a few of them. The complete list is :
+		 *
+		 * -- for org.eclipse.ui.ide :
+		 *
+		 * ./full/obj16/quickfix_info_obj.svg ./full/obj16/quickfix_warning_obj.svg ./full/obj16/lprio_tsk.svg
+		 * ./full/obj16/includeMode_filter.svg ./full/obj16/fileType_filter.svg ./full/obj16/error_tsk.svg
+		 * ./full/obj16/welcome_item.svg ./full/obj16/keyspref_obj.svg ./full/obj16/workset.svg
+		 * ./full/obj16/exportpref_obj.svg ./full/obj16/header_complete.svg ./full/obj16/header_priority.svg
+		 * ./full/obj16/inheritable_filter.svg ./full/obj16/warning.svg ./full/obj16/warn_tsk.svg
+		 * ./full/obj16/folderType_filter.svg ./full/obj16/incomplete_tsk.svg ./full/obj16/fileFolderType_filter.svg
+		 * ./full/obj16/welcome_banner.svg ./full/obj16/importpref_obj.svg ./full/obj16/hprio_tsk.svg
+		 * ./full/obj16/bkmrk_tsk.svg ./full/obj16/quickfix_error_obj.svg ./full/obj16/goto_input.svg
+		 * ./full/obj16/taskmrk_tsk.svg ./full/obj16/prj_obj.svg ./full/obj16/welcome_editor.svg
+		 * ./full/obj16/cprj_obj.svg ./full/obj16/info_tsk.svg ./full/obj16/folder.svg ./full/obj16/complete_tsk.svg
+		 * ./full/obj16/excludeMode_filter.svg ./full/markers/contassist_ovr.svg ./full/markers/help_small.svg
+		 * ./full/wizban/importzip_wiz.svg ./full/wizban/newprj_wiz.svg ./full/wizban/exportdir_wiz.svg
+		 * ./full/wizban/newgroup_wiz.svg ./full/wizban/saveas_wiz.svg ./full/wizban/importdir_wiz.svg
+		 * ./full/wizban/importproj_wiz.svg ./full/wizban/new_wiz.svg ./full/wizban/exportzip_wiz.svg
+		 * ./full/wizban/newfile_wiz.svg ./full/wizban/quick_fix.svg ./full/wizban/workset_wiz.svg
+		 * ./full/wizban/newfolder_wiz.svg ./full/etool16/importzip_wiz.svg ./full/etool16/newprj_wiz.svg
+		 * ./full/etool16/exportdir_wiz.svg ./full/etool16/newgroup_wiz.svg ./full/etool16/importdir_wiz.svg
+		 * ./full/etool16/exportzip_wiz.svg ./full/etool16/newfile_wiz.svg ./full/etool16/prev_nav.svg
+		 * ./full/etool16/newfolder_wiz.svg ./full/etool16/problem_category.svg ./full/etool16/export_wiz.svg
+		 * ./full/etool16/import_wiz.svg ./full/etool16/next_nav.svg ./full/etool16/build_exec.svg
+		 * ./full/etool16/search_src.svg ./full/ovr16/symlink_ovr.svg ./full/ovr16/filterapplied_ovr.svg
+		 * ./full/ovr16/link_ovr.svg ./full/ovr16/linkwarn_ovr.svg ./full/ovr16/virt_ovr.svg
+		 * ./full/eview16/problems_view_info.svg ./full/eview16/bkmrk_nav.svg ./full/eview16/problems_view_error.svg
+		 * ./full/eview16/tasks_tsk.svg ./full/eview16/problems_view.svg ./full/eview16/filenav_nav.svg
+		 * ./full/eview16/problems_view_warning.svg ./full/eview16/pview.svg ./full/elcl16/selected_mode.svg
+		 * ./full/elcl16/cpyqual_menu.svg ./full/elcl16/workingsets.svg ./full/elcl16/addtsk_tsk.svg
+		 * ./full/elcl16/addtoworkset.svg ./full/elcl16/configs.svg ./full/elcl16/showtsk_tsk.svg
+		 * ./full/elcl16/collapseall.svg ./full/elcl16/flatLayout.svg ./full/elcl16/showerr_tsk.svg
+		 * ./full/elcl16/usearch_obj.svg ./full/elcl16/refresh_nav.svg ./full/elcl16/gotoobj_tsk.svg
+		 * ./full/elcl16/filter_ps.svg ./full/elcl16/showcomplete_tsk.svg ./full/elcl16/smartmode_co.svg
+		 * ./full/elcl16/synced.svg ./full/elcl16/hierarchicalLayout.svg ./full/elcl16/showchild_mode.svg
+		 * ./full/elcl16/step_current.svg ./full/elcl16/step_done.svg ./full/elcl16/removefromworkset.svg
+		 * ./full/elcl16/showwarn_tsk.svg
+		 *
+		 * -- for org.eclipse.ui.console:
+		 *
+		 * ./full/cview16/console_view.svg ./full/eview16/console_view.svg ./full/elcl16/lock_co.svg
+		 * ./full/elcl16/wordwrap.svg ./full/elcl16/pin.svg ./full/elcl16/rem_co.svg ./full/elcl16/new_con.svg
+		 * ./full/elcl16/clear_co.svg
+		 *
+		 *
+		 * -- for org.eclipse.ui.views.log:
+		 *
+		 * -- for org.eclipse.ui.workbench.texteditor:
+		 *
+		 * -- for org.eclipse.search:
+		 *
+		 * -- for org.eclipse.ui.browser:
+		 *
+		 * -- for org.eclipse.e4.ui.workbench.renderers.swt:
+		 *
+		 * -- for org.eclipse.ui.views:
+		 *
+		 * -- for org.eclipse.ui:
 		 */
 		final static Map<String, String> DYNAMIC_SUBSTITIONS = Map.ofEntries(entry(
 				"platform:/plugin/org.eclipse.ui.workbench.texteditor/$nl$/icons/full/etool16/block_selection_mode.svg",
@@ -193,7 +251,13 @@ public class Activator extends AbstractUIPlugin {
 		replace(ISharedImages.IMG_OBJS_WARN_TSK, IGamaIcons.MARKER_WARNING);
 		replace(ISharedImages.IMG_OBJS_INFO_TSK, IGamaIcons.MARKER_INFO);
 		replace(ISharedImages.IMG_TOOL_UNDO, IGamaIcons.MENU_UNDO);
+		replace(ISharedImages.IMG_TOOL_UNDO_DISABLED, IGamaIcons.MENU_UNDO, true);
 		replace(ISharedImages.IMG_TOOL_REDO, IGamaIcons.MENU_REDO);
+		replace(ISharedImages.IMG_TOOL_REDO_DISABLED, IGamaIcons.MENU_REDO, true);
+		replace(ISharedImages.IMG_TOOL_BACK, "command.lastedit");
+		replace(ISharedImages.IMG_TOOL_BACK_DISABLED, IGamaIcons.MENU_BACK, true);
+		replace(ISharedImages.IMG_TOOL_FORWARD, "command.nextedit");
+		replace(ISharedImages.IMG_TOOL_FORWARD_DISABLED, "command.nextedit", true);
 		replace(ISharedImages.IMG_OBJ_FILE, IGamaIcons.FILE_TEXT);
 		replace(ISharedImages.IMG_OBJ_FOLDER, IGamaIcons.FOLDER_MODEL);
 		replace(ISharedImages.IMG_LCL_LINKTO_HELP, IGamaIcons.MENU_HELP);
@@ -206,6 +270,13 @@ public class Activator extends AbstractUIPlugin {
 		replace(ISharedImages.IMG_TOOL_COPY, IGamaIcons.COPY);
 		replace(ISharedImages.IMG_TOOL_PASTE, IGamaIcons.PASTE);
 		replace(ISharedImages.IMG_TOOL_DELETE, IGamaIcons.DELETE);
+		replace(ISharedImages.IMG_DEC_FIELD_ERROR, IGamaIcons.OVERLAY_ERROR);
+		replace(ISharedImages.IMG_DEC_FIELD_WARNING, IGamaIcons.OVERLAY_WARNING);
+		replace(ISharedImages.IMG_TOOL_COPY_DISABLED, IGamaIcons.COPY, true);
+		replace(ISharedImages.IMG_TOOL_CUT_DISABLED, IGamaIcons.CUT, true);
+		replace(ISharedImages.IMG_TOOL_PASTE_DISABLED, IGamaIcons.PASTE, true);
+		replace(ISharedImages.IMG_TOOL_DELETE_DISABLED, IGamaIcons.DELETE, true);
+
 	}
 
 	/**
@@ -217,9 +288,27 @@ public class Activator extends AbstractUIPlugin {
 	 *            the gaml image
 	 */
 	void replace(final String id, final String gamlImage) {
+		replace(id, gamlImage, false);
+	}
+
+	/**
+	 * Replace.
+	 *
+	 * @param id
+	 *            the id
+	 * @param gamlImage
+	 *            the gaml image
+	 * @param disabled
+	 *            the disabled
+	 */
+	void replace(final String id, final String gamlImage, final boolean disabled) {
 		JFaceResources.getImageRegistry().remove(id);
-		JFaceResources.getImageRegistry().put(id, GamaIcon.named(gamlImage).image());
-		WorkbenchImages.declareImage(id, GamaIcon.named(gamlImage).descriptor(), true);
+		if (!disabled) {
+			JFaceResources.getImageRegistry().put(id, GamaIcon.named(gamlImage).image());
+			WorkbenchImages.declareImage(id, GamaIcon.named(gamlImage).descriptor(), true);
+		}
+		JFaceResources.getImageRegistry().put(id, GamaIcon.named(gamlImage).disabled());
+		WorkbenchImages.declareImage(id, GamaIcon.named(gamlImage).disabledDescriptor(), true);
 	}
 
 }
