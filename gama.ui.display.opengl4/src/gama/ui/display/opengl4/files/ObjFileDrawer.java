5ad7a350e82a2541b9e47198d55b26efba7d1a14
AlexisDrogoul <579256+AlexisDrogoul@users.noreply.github.com>
2026-03-07 09:06:44 +0000

    Fix remaining opengl4 Eclipse compilation errors

--- gama.ui.display.opengl4/src/gama/ui/display/opengl4/files/ObjFileDrawer.java
+++ gama.ui.display.opengl4/src/gama/ui/display/opengl4/files/ObjFileDrawer.java
@@ -13,8 +13,6 @@ package gama.ui.display.opengl4.files;
 import java.io.File;
 
 import com.jogamp.opengl.GL;
-import com.jogamp.opengl.GL4;
-import com.jogamp.opengl.GL4;
 import com.jogamp.opengl.util.texture.Texture;
 
 import gama.api.types.geometry.GamaPointFactory;
@@ -31,12 +29,23 @@ import gama.ui.display.opengl4.OpenGL;
 public class ObjFileDrawer {
 
 	/**
-	 * Draw to open GL.
-	 *
-	 * @param file
-	 *            the file
-	 * @param gl
-	 *            the gl
+	 * Helper class to encapsulate reusable drawing objects
+	 */
+	private static class DrawContext {
+		final OpenGL gl;
+		final GamaObjFile file;
+		final IPoint tex = GamaPointFactory.create();
+		final IPoint normal = GamaPointFactory.create();
+		final IPoint vertex = GamaPointFactory.create();
+
+		DrawContext(GamaObjFile file, OpenGL gl) {
+			this.file = file;
+			this.gl = gl;
+		}
+	}
+
+	/**
+	 * Handle material loading and texturing
 	 */
 	private static Texture handleMaterial(GamaObjFile file, OpenGL gl, String nextmatname) {
 		gl.setCurrentColor(file.materials.getKd(nextmatname)[0], file.materials.getKd(nextmatname)[1],
@@ -65,7 +74,68 @@ public class ObjFileDrawer {
 		return null;
 	}
 
-		public static void drawToOpenGL(final GamaObjFile file, final OpenGL gl) {
+	/**
+	 * Helper method to draw a single face of the object
+	 */
+	private static void drawFace(final DrawContext ctx, final int faceIndex) {
+		final int[] tempfaces = ctx.file.faces.get(faceIndex);
+		final int[] norms = ctx.file.facesNorms.get(faceIndex);
+		final int[] texs = ctx.file.facesTexs.get(faceIndex);
+
+		final int polytype = tempfaces.length == 3 ? GL.GL_TRIANGLES : GL.GL_TRIANGLE_FAN;
+		ctx.gl.beginDrawing(polytype);
+
+		boolean hasNormals = true;
+		for (int w = 0; w < tempfaces.length; w++) {
+			if (norms[w] == 0) {
+				hasNormals = false;
+				break;
+			}
+		}
+
+		final double[] arrayOfVertices = new double[tempfaces.length * 3];
+		for (int w = 0; w < tempfaces.length; w++) {
+			final double[] ordinates = ctx.file.setOfVertex.get(tempfaces[w] - 1);
+			for (int k = 0; k < 3; k++) { arrayOfVertices[w * 3 + k] = ordinates[k]; }
+		}
+		final ICoordinates coords = GamaCoordinateSequenceFactory.ofLength(tempfaces.length + 1);
+		coords.setTo(arrayOfVertices);
+		coords.completeRing();
+
+		if (!hasNormals) { ctx.gl.setNormal(coords, !coords.isClockwise()); }
+		for (int w = 0; w < tempfaces.length; w++) {
+			if (tempfaces[w] == 0) { continue; }
+			final boolean hasNormal = norms[w] != 0;
+			final boolean hasTex = texs[w] != 0;
+			if (hasNormal) {
+				final double[] temp_coords = ctx.file.setOfVertexNormals.get(norms[w] - 1);
+				ctx.normal.setLocation(temp_coords[0], temp_coords[1], temp_coords[2]);
+			}
+			if (hasTex) {
+				final double[] ordinates = ctx.file.setOfVertexTextures.get(texs[w] - 1);
+				ctx.tex.setLocation(ordinates[0], ordinates[1], ordinates[2]);
+				if (1d >= ctx.tex.getY() && -ctx.tex.getY() <= 0) {
+					ctx.tex.setY(1d - ctx.tex.getY());
+				} else {
+					ctx.tex.setY(Math.abs(ctx.tex.getY()));
+				}
+			}
+			final double[] temp_coords = ctx.file.setOfVertex.get(tempfaces[w] - 1);
+			ctx.vertex.setLocation(temp_coords[0], temp_coords[1], temp_coords[2]);
+			ctx.gl.drawVertex(ctx.vertex, hasNormal ? ctx.normal : null, hasTex ? ctx.tex : null);
+		}
+		ctx.gl.endDrawing();
+	}
+
+	/**
+	 * Draw to open GL.
+	 *
+	 * @param file
+	 *            the file
+	 * @param gl
+	 *            the gl
+	 */
+	public static void drawToOpenGL(final GamaObjFile file, final OpenGL gl) {
 
 		int nextmat = -1;
 		int matcount = 0;
@@ -79,9 +149,8 @@ public class ObjFileDrawer {
 			nextmat = Integer.parseInt(nextmatnamearray[1]);
 		}
 		Texture texture = null;
-		final IPoint tex = GamaPointFactory.create();
-		final IPoint normal = GamaPointFactory.create();
-		final IPoint vertex = GamaPointFactory.create();
+		DrawContext ctx = new DrawContext(file, gl);
+
 		for (int i = 0; i < file.faces.size(); i++) {
 			if (i == nextmat) {
 				if (texture != null) {
@@ -98,90 +167,8 @@ public class ObjFileDrawer {
 					nextmat = Integer.parseInt(nextmatnamearray[1]);
 				}
 			}
-				// gl.getGL().glEnable(GL4.GL_COLOR_MATERIAL);
-				gl.setCurrentColor(file.materials.getKd(nextmatname)[0], file.materials.getKd(nextmatname)[1],
-						file.materials.getKd(nextmatname)[2], file.materials.getd(nextmatname));
 
-				final String mapKa = file.materials.getMapKa(nextmatname);
-				final String mapKd = file.materials.getMapKd(nextmatname);
-				final String mapd = file.materials.getMapd(nextmatname);
-				if (mapKa != null || mapKd != null || mapd != null) {
-					File f = new File(file.mtlPath);
-					StringBuilder path = new StringBuilder().append(f.getAbsolutePath().replace(f.getName(), ""));
-					if (mapd != null) {
-						path.append(mapd);
-					} else if (mapKa != null) {
-						path.append(mapKa);
-					} else if (mapKd != null) { path.append(mapKd); }
-					GamaImageFile asset = new GamaImageFile(null, path.toString());
-					if (asset.exists(null)) {
-						// Solves Issue #1951. Asynchronous loading of textures
-						// was not possible when displaying the file
-						texture = gl.getTexture(asset, false, true);
-						gl.setCurrentTextures(texture.getTextureObject(), texture.getTextureObject());
-						texture.setTexParameteri(gl.getGL(), GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT);
-						texture.setTexParameteri(gl.getGL(), GL.GL_TEXTURE_WRAP_T, GL.GL_REPEAT);
-					}
-
-				}
-				matcount++;
-				if (matcount < totalmats) {
-					nextmatnamearray = file.matTimings.get(matcount);
-					nextmatname = nextmatnamearray[0];
-					nextmat = Integer.parseInt(nextmatnamearray[1]);
-				}
-			}
-
-			final int[] tempfaces = file.faces.get(i);
-			final int[] norms = file.facesNorms.get(i);
-			final int[] texs = file.facesTexs.get(i);
-
-			//// Quad Begin Header ////
-			final int polytype =
-					tempfaces.length == 3 ? GL.GL_TRIANGLES : tempfaces.length == 4 ? GL4.GL_TRIANGLE_FAN : GL4.GL_TRIANGLE_FAN;
-			gl.beginDrawing(polytype);
-			////////////////////////////
-
-			boolean hasNormals = true;
-			for (int w = 0; w < tempfaces.length; w++) {
-				if (norms[w] == 0) {
-					hasNormals = false;
-					break;
-				}
-			}
-
-			final double[] arrayOfVertices = new double[tempfaces.length * 3];
-			for (int w = 0; w < tempfaces.length; w++) {
-				final double[] ordinates = file.setOfVertex.get(tempfaces[w] - 1);
-				for (int k = 0; k < 3; k++) { arrayOfVertices[w * 3 + k] = ordinates[k]; }
-			}
-			final ICoordinates coords = GamaCoordinateSequenceFactory.ofLength(tempfaces.length + 1);
-			coords.setTo(arrayOfVertices);
-			coords.completeRing();
-
-			if (!hasNormals) { gl.setNormal(coords, !coords.isClockwise()); }
-			for (int w = 0; w < tempfaces.length; w++) {
-				if (tempfaces[w] == 0) { continue; }
-				final boolean hasNormal = norms[w] != 0;
-				final boolean hasTex = texs[w] != 0;
-				if (hasNormal) {
-					final double[] temp_coords = file.setOfVertexNormals.get(norms[w] - 1);
-					normal.setLocation(temp_coords[0], temp_coords[1], temp_coords[2]);
-				}
-				if (hasTex) {
-					final double[] ordinates = file.setOfVertexTextures.get(texs[w] - 1);
-					tex.setLocation(ordinates[0], ordinates[1], ordinates[2]);
-					if (1d >= tex.getY() && -tex.getY() <= 0) {
-						tex.setY(1d - tex.getY());
-					} else {
-						tex.setY(Math.abs(tex.getY()));
-					}
-				}
-				final double[] temp_coords = file.setOfVertex.get(tempfaces[w] - 1);
-				vertex.setLocation(temp_coords[0], temp_coords[1], temp_coords[2]);
-				gl.drawVertex(vertex, hasNormal ? normal : null, hasTex ? tex : null);
-			}
-			gl.endDrawing();
+			drawFace(ctx, i);
 		}
 	}
 
--- gama.ui.display.opengl4/src/gama/ui/display/opengl4/view/NEWTLayeredDisplayMultiListener.java
+++ gama.ui.display.opengl4/src/gama/ui/display/opengl4/view/NEWTLayeredDisplayMultiListener.java
@@ -35,6 +35,24 @@ import gama.ui.experiment.views.displays.LayeredDisplayMultiListener;
  *
  */
 public class NEWTLayeredDisplayMultiListener implements MouseListener, KeyListener, WindowListener, IDisposable {
+	private int mapKeyCodeToEventAction(int keyCode) {
+		return switch (keyCode) {
+			case KeyEvent.VK_UP -> IEventLayerListener.ARROW_UP;
+			case KeyEvent.VK_DOWN -> IEventLayerListener.ARROW_DOWN;
+			case KeyEvent.VK_LEFT -> IEventLayerListener.ARROW_LEFT;
+			case KeyEvent.VK_RIGHT -> IEventLayerListener.ARROW_RIGHT;
+			case KeyEvent.VK_PAGE_UP -> IEventLayerListener.KEY_PAGE_UP;
+			case KeyEvent.VK_PAGE_DOWN -> IEventLayerListener.KEY_PAGE_DOWN;
+			case KeyEvent.VK_ESCAPE -> IEventLayerListener.KEY_ESC;
+			case KeyEvent.VK_ENTER -> IEventLayerListener.KEY_RETURN;
+			case KeyEvent.VK_TAB -> IEventLayerListener.KEY_TAB;
+			case KeyEvent.VK_SHIFT -> IEventLayerListener.KEY_SHIFT;
+			case KeyEvent.VK_ALT -> IEventLayerListener.KEY_ALT;
+			case KeyEvent.VK_CONTROL -> IEventLayerListener.KEY_CTRL;
+			case KeyEvent.VK_META -> IEventLayerListener.KEY_CMD;
+			default -> 0;
+		};
+	}
 
 	static {
 		DEBUG.OFF();
