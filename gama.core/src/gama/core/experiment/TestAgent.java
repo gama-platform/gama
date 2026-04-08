8b07b599cdc3f53117504969802e6deab9766520
chapuisk <kevin.chapuis@gmail.com>
2026-04-08 11:42:54 +0700

    Separate execution of test experiment from batch experiment

--- gama.core/src/gama/core/experiment/TestAgent.java
+++ gama.core/src/gama/core/experiment/TestAgent.java
@@ -9,11 +9,8 @@
  ********************************************************************************************************/
 package gama.core.experiment;
 
-import static gama.api.gaml.types.Cast.asFloat;
-
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 
 import org.eclipse.emf.common.util.URI;
@@ -24,17 +21,13 @@ import gama.annotations.constants.IKeyword;
 import gama.api.exceptions.GamaRuntimeException;
 import gama.api.gaml.expressions.IExpression;
 import gama.api.gaml.statements.IStatement;
-import gama.api.gaml.symbols.IParameter;
 import gama.api.gaml.types.IType;
 import gama.api.gaml.types.Types;
 import gama.api.kernel.agent.IPopulation;
-import gama.api.kernel.species.IExperimentSpecies;
 import gama.api.runtime.scope.IScope;
 import gama.api.utils.tests.ITestAgent;
 import gama.api.utils.tests.TestExperimentSummary;
 import gama.api.utils.tests.WithTestSummary;
-import gama.core.experiment.batch.BatchAgent;
-import gama.core.experiment.parameters.ParameterAdapter;
 
 /**
  * The Class TestAgent.
@@ -42,7 +35,7 @@ import gama.core.experiment.parameters.ParameterAdapter;
 @experiment (IKeyword.TEST)
 @doc ("Experiments supporting the collection of success or failure of tests. Can be used in GUI or headless")
 @SuppressWarnings ({ "unchecked", "rawtypes" })
-public class TestAgent extends BatchAgent implements ITestAgent {
+public class TestAgent extends ExperimentAgent implements ITestAgent {
 
 	/** The summary. */
 	// int failedModels = 0;
@@ -108,54 +101,6 @@ public class TestAgent extends BatchAgent implements ITestAgent {
 		getScope().getGui().displayTestsResults(getScope(), summary);
 		getScope().getGui().endTestDisplay();
 		super.dispose();
-	}
-
-	@Override
-	protected String endStatus() {
-		return "Tests over: " + summary.getStringSummary();
-	}
-
-	@Override
-	public void addSpecificParameters(final List<IParameter.Batch> params) {
-		params.add(new ParameterAdapter("Stop condition", IExperimentSpecies.TEST_CATEGORY_NAME, IType.STRING) {
-
-			@Override
-			public String value() {
-				return stopCondition != null ? stopCondition.serializeToGaml(false) : "none";
-			}
-
-		});
-
-		params.add(new ParameterAdapter("Parameter space", IExperimentSpecies.TEST_CATEGORY_NAME, "", IType.STRING) {
-
-			@Override
-			public String value() {
-				final Map<String, IParameter.Batch> explorable = getSpecies().getExplorableParameters();
-				if (explorable.isEmpty()) return "1";
-				String result = "";
-				int dim = 1;
-				for (final Map.Entry<String, IParameter.Batch> entry : explorable.entrySet()) {
-					result += entry.getKey() + " (";
-					final int entryDim = getExplorationDimension(entry.getValue());
-					dim = dim * entryDim;
-					result += String.valueOf(entryDim) + ") * ";
-				}
-				if (!result.isEmpty()) { result = result.substring(0, result.length() - 2); }
-				result += " = " + dim;
-				return result;
-			}
-
-			int getExplorationDimension(final IParameter.Batch p) {
-				IScope scope = getScope();
-				// AD TODO Issue a warning in the compilation if a batch experiment tries to explore non-int or
-				// non-float values
-				if (p.getAmongValue(scope) != null) return p.getAmongValue(scope).size();
-				return (int) ((asFloat(scope, p.getMaxValue(scope)) - asFloat(scope, p.getMinValue(scope)))
-						/ asFloat(scope, p.getStepValue(scope))) + 1;
-			}
-
-		});
-
 	}
 
 	/**
