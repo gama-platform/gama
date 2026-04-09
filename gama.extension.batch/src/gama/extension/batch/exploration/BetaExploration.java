f923c892719d13b6fa1d4147d97d5e66ad5d3894
chapuisk <kevin.chapuis@gmail.com>
2026-04-09 17:32:23 +0700

    [BATCH] creation of a batch plugin to host everything about batch exploration and calibration

---
+++ gama.extension.batch/src/gama/extension/batch/exploration/BetaExploration.java
@@ -0,0 +1,289 @@
+/*******************************************************************************************************
+ *
+ * BetaExploration.java, in gama.core, is part of the source code of the GAMA modeling and simulation platform
+ * (v.2025-03).
+ *
+ * (c) 2007-2026 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
+ *
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
+ *
+ ********************************************************************************************************/
+package gama.extension.batch.exploration;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+
+import gama.annotations.doc;
+import gama.annotations.example;
+import gama.annotations.facet;
+import gama.annotations.facets;
+import gama.annotations.inside;
+import gama.annotations.symbol;
+import gama.annotations.usage;
+import gama.annotations.constants.IKeyword;
+import gama.annotations.support.IConcept;
+import gama.annotations.support.ISymbolKind;
+import gama.api.compilation.descriptions.IDescription;
+import gama.api.exceptions.GamaRuntimeException;
+import gama.api.gaml.symbols.IParameter.Batch;
+import gama.api.gaml.symbols.ISymbol;
+import gama.api.gaml.types.Cast;
+import gama.api.gaml.types.IType;
+import gama.api.kernel.simulation.IExperimentAgent;
+import gama.api.kernel.simulation.IExploration;
+import gama.api.runtime.scope.IScope;
+import gama.api.types.list.IList;
+import gama.api.types.map.GamaMapFactory;
+import gama.api.types.map.IMap;
+import gama.api.utils.StringUtils;
+import gama.api.utils.files.FileUtils;
+import gama.api.utils.prefs.GamaPreferences;
+import gama.core.experiment.parameters.ParametersSet;
+
+/**
+ *
+ *
+ * @author kevinchapuis
+ *
+ *         Coefficient derived from the work below:
+ *
+ *         E. Borgonovo, M. Pangallo, J. Rivkin, L. Rizzo, and N. Siggelkow, “Sensitivity analysis of agent-based
+ *         models: a new protocol,” Comput. Math. Organ. Theory, vol. 28, no. 1, pp. 52–94, Mar. 2022, doi:
+ *         10.1007/s10588-021-09358-5.
+ *
+ */
+@symbol (
+		name = IExploration.BETAD,
+		kind = ISymbolKind.BATCH_METHOD,
+		with_sequence = false,
+		concept = { IConcept.BATCH, IConcept.ALGORITHM })
+@inside (
+		kinds = { ISymbolKind.EXPERIMENT })
+@facets (
+		value = { @facet (
+				name = IKeyword.NAME,
+				type = IType.ID,
+				optional = false,
+				internal = true,
+				doc = @doc ("The name of the method. For internal use only")),
+				@facet (
+						name = Exploration.SAMPLING,
+						type = IType.ID,
+						optional = false,
+						doc = @doc ("The SAMPLING method to build parameters sets that must be factorial based to some extends - available are: "
+								+ IKeyword.LHS + ", " + IKeyword.ORTHOGONAL + ", " + IKeyword.FACTORIAL + ", "
+								+ IKeyword.UNIFORM + ", " + IKeyword.SALTELLI + ", " + IExploration.MORRIS)),
+				@facet (
+						name = IKeyword.BATCH_VAR_OUTPUTS,
+						type = IType.LIST,
+						optional = false,
+						doc = @doc ("The list of output variables to analyse")),
+				@facet (
+						name = Exploration.SAMPLE_SIZE,
+						type = IType.INT,
+						optional = true,
+						doc = @doc ("The number of sample required.")),
+				@facet (
+						name = BetaExploration.BOOTSTRAP,
+						type = IType.INT,
+						optional = true,
+						doc = @doc ("The number of time each parameter value is boostraped (or resampled in another context)")),
+				@facet (
+						name = IKeyword.BATCH_OUTPUT,
+						type = IType.STRING,
+						optional = true,
+						doc = @doc ("The path to the file where the automatic batch report will be written")),
+				@facet (
+						name = IKeyword.BATCH_REPORT,
+						type = IType.STRING,
+						optional = false,
+						doc = @doc ("The path to the file where the Betad report will be written")) },
+		omissible = IKeyword.NAME)
+@doc (
+		value = "This algorithm runs an exploration with a given SAMPLING to compute BetadKu - see doi: 10.1007/s10588-021-09358-5",
+		usages = { @usage (
+				value = "For example: ",
+				examples = { @example (
+						value = "method sobol sample_size:100 outputs:['my_var'] report:'../path/to/report/file.txt'; ",
+						isExecutable = false) }) })
+public class BetaExploration extends AExplorationAlgorithm {
+
+	/** The bootstrap. */
+	private int bootstrap = Betadistribution.DEFAULT_BOOTSTRAP;
+
+	/** The Constant BOOTSTRAP. */
+	public static final String BOOTSTRAP = "bootstrap";
+
+	/** Theoretical inputs */
+	private List<Batch> parameters;
+	/** Theoretical outputs */
+	private IList<String> outputs;
+	/** Actual input / output map */
+	protected IMap<ParametersSet, Map<String, List<Object>>> res_outputs;
+
+	/**
+	 * Instantiates a new beta exploration.
+	 *
+	 * @param desc
+	 *            the desc
+	 */
+	public BetaExploration(final IDescription desc) {
+		super(desc);
+	}
+
+	@Override
+	public void setChildren(final Iterable<? extends ISymbol> children) {}
+
+	@SuppressWarnings ("unchecked")
+	@Override
+	public void explore(final IScope scope) {
+
+		parameters = parameters == null ? getParams(currentExperiment) : parameters;
+
+		if (hasFacet(IExploration.SAMPLE_SIZE)) {
+			sample_size = Cast.asInt(scope, getFacet(IExploration.SAMPLE_SIZE).value(scope));
+		}
+		if (sample_size < 1) { sample_size = 2; }
+
+		// == Build sample of parameter inputs ==
+		List<ParametersSet> sets = getExperimentPlan(parameters, scope);
+
+		// TODO : expend parameter set to include variation over target input,
+		// ====> i.e. various parameter combinations for one parameter value
+		// to assess how simulation behave when a parameter stay the same, while everything
+		// else is moving
+		sets = expendExperimentPlan(sets, scope);
+
+		// TODO : why doesn't it take into account the value of 'keep_simulations:' ?
+		currentExperiment.setKeepSimulations(false);
+		res_outputs = currentExperiment.runSimulationsAndReturnResults(sets);
+
+		outputs = getLitteralOutputs();
+
+		Map<String, Map<Batch, Double>> res = new HashMap<>();
+		for (String out : outputs) {
+			IMap<ParametersSet, List<Object>> sp = GamaMapFactory.create();
+			for (ParametersSet ps : res_outputs.keySet()) { sp.put(ps, res_outputs.get(ps).get(out)); }
+			Betadistribution bs = new Betadistribution(sp, parameters);
+			res.put(out, bs.evaluate());
+		}
+
+		/* Save the simulation values in the provided .csv file (input and corresponding output) */
+		if (hasFacet(IKeyword.BATCH_OUTPUT)) { saveRawResults(scope, res_outputs); }
+
+		String path_to = Cast.asString(scope, getFacet(IKeyword.BATCH_REPORT).value(scope));
+		final File f = new File(FileUtils.constructAbsoluteFilePath(scope, path_to, false));
+		final File parent = f.getParentFile();
+		if (!parent.exists()) { parent.mkdirs(); }
+		if (f.exists()) { f.delete(); }
+		try (FileWriter fw = new FileWriter(f, false)) {
+			fw.write(buildReportString(res, FilenameUtils.getExtension(f.getPath())));
+		} catch (Exception e) {
+			throw GamaRuntimeException.error("File " + f.toString() + " not found", scope);
+		}
+
+	}
+
+	@Override
+	public void addParametersTo(final List<Batch> exp, final IExperimentAgent.Batch agent) {
+		super.addParametersTo(exp, agent);
+	}
+
+	// ================================== //
+
+	/**
+	 * Get back the list of 'explorable' parameters (numerical variable with min and max values)
+	 *
+	 * @param xp
+	 * @return
+	 */
+	private List<Batch> getParams(final IExperimentAgent.Batch xp) {
+		return xp.getParametersToExplore().stream().map(p -> p).toList();
+	}
+
+	/**
+	 * Duplicates values of parameter to put them in various context
+	 *
+	 * @param sets
+	 * @param scope
+	 * @return
+	 */
+	private List<ParametersSet> expendExperimentPlan(final List<ParametersSet> sets, final IScope scope) {
+
+		List<ParametersSet> returnedSet = new ArrayList<>(sets);
+
+		if (hasFacet(BOOTSTRAP)) { bootstrap = Cast.asInt(scope, getFacet(BOOTSTRAP).value(scope)); }
+
+		// For each parameter, duplicates 'fact' times all sampled values
+		for (Batch b : parameters) {
+
+			// Target the parameter values and put them in another parameter context
+			for (ParametersSet ps : sets) {
+				List<ParametersSet> subspace = new ArrayList<>(sets);
+				subspace.remove(ps);
+				for (int i = 0; i < bootstrap; i++) {
+					ParametersSet cross =
+							new ParametersSet(subspace.remove((int) scope.getRandom().next() * subspace.size()));
+					cross.addValueAtIndex(scope, b, ps.get(b.getName()));
+					returnedSet.add(cross);
+				}
+			}
+		}
+
+		return returnedSet;
+	}
+
+	// ================================== //
+
+	/**
+	 * Builds the report string.
+	 *
+	 * @param res
+	 *            the res
+	 * @return the string
+	 */
+	public String buildReportString(final Map<String, Map<Batch, Double>> res, final String extension) {
+		StringBuilder sb = new StringBuilder();
+		var separator = GamaPreferences.External.CSV_SEPARATOR.getValue();
+		if ("txt".equalsIgnoreCase(extension)) {
+
+			sb.append("BETA b Kuiper based estimator :").append(StringUtils.LN);
+			sb.append("##############################").append(StringUtils.LN);
+			sb.append("inputs" + separator + String.join(separator, outputs))
+					.append(StringUtils.LN);
+			for (Batch param : parameters) {
+				sb.append(param.getName());
+				for (String output_name : outputs) {
+					sb.append(separator).append(res.get(output_name).get(param));
+				}
+				sb.append(StringUtils.LN);
+			}
+
+		} else {
+
+			// Build header
+			sb.append("output").append(separator);
+			sb.append("parameter").append(separator);
+			sb.append("\u03B2").append(StringUtils.LN);
+
+			for (String output_name : outputs) {
+				for (Batch param : parameters) {
+					// The output & parameter
+					sb.append(output_name).append(separator);
+					sb.append(param.getName()).append(separator);
+					sb.append(res.get(output_name).get(param)).append(StringUtils.LN);
+				}
+			}
+
+		}
+
+		return sb.toString();
+	}
+
+}
---
+++ gama.extension.batch/src/gama/extension/batch/optimization/AOptimizationAlgorithm.java
@@ -0,0 +1,377 @@
+/*******************************************************************************************************
+ *
+ * AOptimizationAlgorithm.java, in gama.core, is part of the source code of the GAMA modeling and simulation platform
+ * (v.2025-03).
+ *
+ * (c) 2007-2026 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, ESPACE-DEV, CTU)
+ *
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
+ *
+ ********************************************************************************************************/
+package gama.extension.batch.optimization;
+
+import static gama.api.gaml.types.Cast.asFloat;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import gama.annotations.inside;
+import gama.annotations.constants.IKeyword;
+import gama.annotations.support.ISymbolKind;
+import gama.api.GAMA;
+import gama.api.compilation.descriptions.IDescription;
+import gama.api.exceptions.GamaRuntimeException;
+import gama.api.gaml.GAML;
+import gama.api.gaml.expressions.IExpression;
+import gama.api.gaml.symbols.IParameter;
+import gama.api.gaml.symbols.ISymbol;
+import gama.api.gaml.symbols.Symbol;
+import gama.api.gaml.types.IType;
+import gama.api.kernel.simulation.IExperimentAgent;
+import gama.api.kernel.simulation.IExploration;
+import gama.api.kernel.species.IExperimentSpecies;
+import gama.api.runtime.scope.IScope;
+import gama.api.runtime.scope.InScope;
+import gama.core.experiment.parameters.ParameterAdapter;
+import gama.core.experiment.parameters.ParametersSet;
+import gama.extension.batch.BatchAgent;
+import gama.extension.batch.exploration.AExplorationAlgorithm;
+
+/**
+ * The Class AOptimizationAlgorithm.
+ */
+@inside (
+		kinds = { ISymbolKind.EXPERIMENT })
+public abstract class AOptimizationAlgorithm extends Symbol implements IExploration {
+
+	/** The Constant C_MEAN. */
+	public final static short C_MAX = 0, C_MIN = 1, C_MEAN = 2;
+
+	/** The Constant COMBINATIONS. */
+	public final static String[] COMBINATIONS = { "maximum", "minimum", "average" };
+	static {
+		GAML.addConstants(COMBINATIONS);
+	}
+
+	/** The tested solutions. */
+	protected HashMap<ParametersSet, Double> testedSolutions;
+
+	/** The fitness expression. */
+	protected IExpression fitnessExpression;
+
+	/** The is maximize. */
+	protected boolean isMaximize;
+
+	/** The current experiment. */
+	// private BatchAgent currentExperiment;
+
+	/** The best solution. */
+	protected ParametersSet bestSolution = null;
+
+	/** The best fitness. */
+	protected Double bestFitness = null;
+
+	/** The combination. */
+	protected short combination;
+
+	/**
+	 * Find best solution.
+	 *
+	 * @param scope
+	 *            the scope
+	 * @return the parameters set
+	 * @throws GamaRuntimeException
+	 *             the gama runtime exception
+	 */
+	protected abstract ParametersSet findBestSolution(IScope scope) throws GamaRuntimeException;
+
+	/**
+	 * Initialize for.
+	 *
+	 * @param scope
+	 *            the scope
+	 * @param agent
+	 *            the agent
+	 * @throws GamaRuntimeException
+	 *             the gama runtime exception
+	 */
+	@Override
+	public void initializeFor(final IScope scope, final IExperimentAgent.Batch agent) throws GamaRuntimeException {
+		// setCurrentExperiment(agent);
+	}
+
+	/**
+	 * Initialize tested solutions.
+	 */
+	protected void initializeTestedSolutions() {
+		testedSolutions = new HashMap<>();
+	}
+
+	/**
+	 * Inits the params.
+	 */
+	protected void initParams() {
+		GAMA.run(new InScope.Void() {
+
+			@Override
+			public void process(final IScope scope) {
+				initParams(scope);
+			}
+		});
+	}
+
+	/**
+	 * Inits the params.
+	 *
+	 * @param scope
+	 *            the scope
+	 */
+	protected void initParams(final IScope scope) {}
+
+	/**
+	 * Instantiates a new a optimization algorithm.
+	 *
+	 * @param desc
+	 *            the desc
+	 */
+	public AOptimizationAlgorithm(final IDescription desc) {
+		super(desc);
+		initializeTestedSolutions();
+		fitnessExpression = getFacet(IKeyword.MAXIMIZE, IKeyword.MINIMIZE);
+		isMaximize = hasFacet(IKeyword.MAXIMIZE);
+		final String ag = getLiteral(IKeyword.AGGREGATION);
+		combination = IKeyword.MAX.equals(ag) ? C_MAX : IKeyword.MIN.equals(ag) ? C_MIN : C_MEAN;
+		bestFitness = isMaximize ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
+	}
+
+	@Override
+	public void run(final IScope scope) {
+		try {
+			findBestSolution(scope);
+		} catch (final GamaRuntimeException e) {
+			GAMA.reportError(scope, e, false);
+		}
+	}
+
+	@Override
+	public void setChildren(final Iterable<? extends ISymbol> commands) {}
+
+	@Override
+	public void addParametersTo(final List<IParameter.Batch> params, final IExperimentAgent.Batch agent) {
+
+		params.add(new ParameterAdapter("Parameter space", BatchAgent.CALIBRATION_EXPERIMENT, "", IType.STRING) {
+
+			@Override
+			public String value() {
+				BatchAgent batch = getCurrentExperiment();
+				if (batch == null) return "-";
+				final Map<String, IParameter.Batch> explorable = batch.getSpecies().getExplorableParameters();
+				if (explorable.isEmpty()) return "1";
+				String result = "";
+				int dim = 1;
+				for (final Map.Entry<String, IParameter.Batch> entry : explorable.entrySet()) {
+					result += entry.getKey() + " (";
+					final int entryDim = getExplorationDimension(entry.getValue());
+					dim = dim * entryDim;
+					result += String.valueOf(entryDim) + ") * ";
+				}
+				result = result.substring(0, result.length() - 2);
+				result += " = " + dim;
+				return result;
+			}
+
+			int getExplorationDimension(final IParameter.Batch p) {
+				BatchAgent batch = getCurrentExperiment();
+				IScope scope = batch == null ? GAMA.getRuntimeScope() : batch.getScope();
+
+				// AD TODO Issue a warning in the compilation if a batch experiment tries to explore non-int or
+				// non-float values
+				if (p.getAmongValue(scope) != null) return p.getAmongValue(scope).size();
+				return (int) ((asFloat(scope, p.getMaxValue(scope)) - asFloat(scope, p.getMinValue(scope)))
+						/ asFloat(scope, p.getStepValue(scope))) + 1;
+			}
+
+		});
+
+		params.add(
+				new ParameterAdapter("Last parameter set tested", BatchAgent.CALIBRATION_EXPERIMENT, "", IType.STRING) {
+
+					@Override
+					public String value() {
+						BatchAgent batch = getCurrentExperiment();
+						if (batch == null || batch.getLatestSolution() == null) return "-";
+						return batch.getLatestSolution().toString();
+					}
+
+				});
+
+		params.add(new ParameterAdapter("Calibration method", BatchAgent.CALIBRATION_EXPERIMENT, IType.STRING) {
+
+			@Override
+			public Object value() {
+				@SuppressWarnings ("rawtypes") final List<Class<?>> classes = AExplorationAlgorithm.CLASSES;
+				final String methodName = IExploration.METHODS[classes.indexOf(AOptimizationAlgorithm.this.getClass())];
+				final String fit = fitnessExpression == null ? "" : "fitness = "
+						+ (isMaximize ? " maximize " : " minimize ") + fitnessExpression.serializeToGaml(false);
+				final String sim = fitnessExpression == null ? ""
+						: (combination == C_MAX ? " max " : combination == C_MIN ? " min " : " average ") + "of "
+								+ agent.getSeeds().length + " simulations";
+				return "Method " + methodName + " | " + fit + " | " + "compute the" + sim + " for each solution";
+			}
+
+		});
+
+		params.add(
+				new ParameterAdapter("Best parameter set found", BatchAgent.CALIBRATION_EXPERIMENT, "", IType.STRING) {
+
+					@Override
+					public String value() {
+						final ParametersSet solutions = bestSolution;
+						if (solutions == null) return "";
+						return solutions.toString();
+					}
+
+				});
+
+		params.add(new ParameterAdapter("Best fitness", BatchAgent.CALIBRATION_EXPERIMENT, "", IType.STRING) {
+
+			@Override
+			public String value() {
+				final Double best = bestFitness;
+				if (best == null) return "-";
+				return best.toString();
+			}
+
+		});
+
+	}
+
+	@Override
+	public boolean isFitnessBased() { return true; }
+
+	@Override
+	public IExpression getOutputs() { return getFitnessExpression(); }
+
+	// ------------
+	// OPTIMIZATION
+
+	/**
+	 * Return the best fitness of the experiment
+	 *
+	 * @return Double
+	 */
+	public Double getBestFitness() { return bestFitness; }
+
+	/**
+	 * Return the expression that characterizes the fitness computation
+	 *
+	 * @return IExpression
+	 */
+	public IExpression getFitnessExpression() { return fitnessExpression; }
+
+	/**
+	 * Return the set of parameter @ParametersSet attached to the best fitness
+	 *
+	 * @return ParametersSet
+	 */
+	public ParametersSet getBestSolution() { return bestSolution; }
+
+	/**
+	 * If the fitness should maximize (or minimize) the corresponding value
+	 *
+	 * @return boolean
+	 */
+	public boolean getIsMaximize() { return this.isMaximize; }
+
+	/**
+	 * Returns the way to combine replication fitness (either min, max or mean)
+	 *
+	 * @return short
+	 */
+	public short getCombination() { return combination; }
+
+	/**
+	 * Checks if is maximize.
+	 *
+	 * @return true, if is maximize
+	 */
+	public boolean isMaximize() { return isMaximize; }
+
+	/**
+	 * Gets the combination name.
+	 *
+	 * @return the combination name
+	 */
+	public String getCombinationName() { return COMBINATIONS[combination]; }
+
+	/**
+	 * Sets the best solution.
+	 *
+	 * @param bestSolution
+	 *            the new best solution
+	 */
+	protected void setBestSolution(final ParametersSet bestSolution) {
+		this.bestSolution = new ParametersSet(bestSolution);
+	}
+
+	/**
+	 * Sets the best fitness.
+	 *
+	 * @param bestFitness
+	 *            the new best fitness
+	 */
+	protected void setBestFitness(final Double bestFitness) { this.bestFitness = bestFitness; }
+
+	/**
+	 * Update best fitness.
+	 *
+	 * @param solution
+	 *            the solution
+	 * @param fitness
+	 *            the fitness
+	 */
+	public void updateBestFitness(final ParametersSet solution, final Double fitness) {
+		if (fitness == null) return;
+		Double best = getBestFitness();
+		if (bestSolution == null || (isMaximize() ? fitness > best : fitness < best)) {
+			setBestFitness(fitness);
+			setBestSolution(solution);
+		}
+	}
+
+	/**
+	 * Gets the current experiment.
+	 *
+	 * @author Alexis Drogoul (alexis.drogoul@ird.fr)
+	 * @return the current experiment
+	 * @date 1 sept. 2023
+	 */
+	protected BatchAgent getCurrentExperiment() {
+		IExperimentSpecies plan = GAMA.getExperiment();
+		if (plan == null) return null; // can happen when closing
+		IExperimentAgent agent = plan.getAgent();
+		if (agent instanceof BatchAgent batch) return batch;
+		return null;
+	}
+
+	/**
+	 * Gets the first fitness.
+	 *
+	 * @author Alexis Drogoul (alexis.drogoul@ird.fr)
+	 * @param results
+	 *            the results
+	 * @return the first fitness
+	 * @date 1 sept. 2023
+	 */
+	protected Double getFirstFitness(final Map<String, List<Object>> results) {
+		List<Object> objects = results.get(IKeyword.FITNESS);
+		if (objects == null || objects.isEmpty()) return 0d;
+		Object o = objects.get(0);
+		return o instanceof Double d ? d : 0d;
+	}
+
+	// protected void setCurrentExperiment(BatchAgent currentExperiment) {
+	// this.currentExperiment = currentExperiment;
+	// }
+}
