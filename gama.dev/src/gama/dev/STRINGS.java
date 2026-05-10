d0d7e82c4099dad10e65c03aaca717e69e2a59d7
AlexisDrogoul <alexis.drogoul@gmail.com>
2026-05-10 15:22:45 +0700

    Enhance TO_STRING for arrays, iterables, and cycles
    Extend STRINGS.TO_STRING to safely stringify primitive and object arrays, Iterables, Enumerations and Iterators with cycle detection (IdentityHashMap) to avoid infinite recursion. Add helper methods (ARRAY_TO_STRING, ENUMERATION_TO_STRING, ITERATOR_TO_STRING) and an overloaded private TO_STRING that tracks visited containers; update imports and Javadoc accordingly. Also tighten PAD method formatting and small doc/comment cleanups.

--- gama.dev/src/gama/dev/STRINGS.java
+++ gama.dev/src/gama/dev/STRINGS.java
@@ -1,7 +1,6 @@
 /*******************************************************************************************************
  *
- * STRINGS.java, in gama.annotations, is part of the source code of the GAMA modeling and simulation platform
- * .
+ * STRINGS.java, in gama.annotations, is part of the source code of the GAMA modeling and simulation platform .
  *
  * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
@@ -11,15 +10,20 @@
 package gama.dev;
 
 import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 /**
- * The Class STRINGS.
+ * Utility methods used to obtain readable string representations and padded strings.
  */
 public class STRINGS {
 
-	/** The Constant TO_STRING. */
+	/**
+	 * Stringifiers dedicated to primitive arrays, keyed by their component type.
+	 */
 	static final ConcurrentHashMap<Class<?>, Function<Object, String>> TO_STRING = new ConcurrentHashMap<>();
 
 	static {
@@ -34,33 +38,125 @@ public class STRINGS {
 	}
 
 	/**
-	 * Tries to obtain a correct string representation of the object, including when
-	 * it is an array (or an array of arrays). Made public to be used outside the
-	 * debug sessions
+	 * Tries to obtain a correct string representation of the object, including when it is an array (or an array of
+	 * arrays), an {@link Iterable}, or an {@link Enumeration}. Made public to be used outside the debug sessions.
 	 *
-	 * @param object any object
+	 * @param object
+	 *            any object
 	 * @return its string representation
 	 */
 	public static String TO_STRING(final Object object) {
-		if (object == null)
-			return "null";
-		if (object.getClass().isArray()) {
-			final Class<?> clazz = object.getClass().getComponentType();
-			if (clazz.isPrimitive())
-				return TO_STRING.get(clazz).apply(object);
-			return Arrays.deepToString((Object[]) object);
-		}
-		return object.toString();
-
+		return TO_STRING(object, new IdentityHashMap<>());
 	}
 
 	/**
-	 * A utility method for padding a string with spaces in order to obtain a length
-	 * of "minLength"
+	 * Builds a readable string representation of the specified object while keeping track of already visited container
+	 * instances to avoid infinite recursion.
 	 *
-	 * @param string    the string to pad
-	 * @param minLength the minimum length to reach (if the string is longer, it
-	 *                  will be return as is)
+	 * @param object
+	 *            the object to stringify
+	 * @param visited
+	 *            the container instances already being stringified
+	 * @return a readable string representation of the object
+	 */
+	private static String TO_STRING(final Object object, final IdentityHashMap<Object, Boolean> visited) {
+		if (object == null) return "null";
+		if (object.getClass().isArray()) {
+			if (visited.put(object, Boolean.TRUE) != null) return "[...]";
+			final Class<?> clazz = object.getClass().getComponentType();
+			try {
+				if (clazz.isPrimitive()) return TO_STRING.get(clazz).apply(object);
+				return ARRAY_TO_STRING((Object[]) object, visited);
+			} finally {
+				visited.remove(object);
+			}
+		}
+		if (object instanceof Iterable<?> iterable) {
+			if (visited.put(object, Boolean.TRUE) != null) return "[...]";
+			try {
+				return ITERATOR_TO_STRING(iterable.iterator(), visited);
+			} finally {
+				visited.remove(object);
+			}
+		}
+		if (object instanceof Enumeration<?> enumeration) {
+			if (visited.put(object, Boolean.TRUE) != null) return "[...]";
+			try {
+				return ENUMERATION_TO_STRING(enumeration, visited);
+			} finally {
+				visited.remove(object);
+			}
+		}
+
+		return object.toString();
+	}
+
+	/**
+	 * Builds a string representation of an object array and recursively stringifies its elements.
+	 *
+	 * @param array
+	 *            the array to stringify
+	 * @param visited
+	 *            the container instances already being stringified
+	 * @return the string representation of the array
+	 */
+	private static String ARRAY_TO_STRING(final Object[] array, final IdentityHashMap<Object, Boolean> visited) {
+		final StringBuilder sb = new StringBuilder().append('[');
+		for (int i = 0; i < array.length; i++) {
+			if (i > 0) { sb.append(", "); }
+			sb.append(TO_STRING(array[i], visited));
+		}
+		return sb.append(']').toString();
+	}
+
+	/**
+	 * Builds a string representation of an {@link Enumeration} and recursively stringifies its elements.
+	 *
+	 * @param enumeration
+	 *            the enumeration to stringify
+	 * @param visited
+	 *            the container instances already being stringified
+	 * @return the string representation of the enumeration
+	 */
+	private static String ENUMERATION_TO_STRING(final Enumeration<?> enumeration,
+			final IdentityHashMap<Object, Boolean> visited) {
+		final StringBuilder sb = new StringBuilder().append('[');
+		boolean first = true;
+		while (enumeration.hasMoreElements()) {
+			if (!first) { sb.append(", "); }
+			sb.append(TO_STRING(enumeration.nextElement(), visited));
+			first = false;
+		}
+		return sb.append(']').toString();
+	}
+
+	/**
+	 * Builds a string representation of an {@link Iterator} and recursively stringifies its elements.
+	 *
+	 * @param iterator
+	 *            the iterator to stringify
+	 * @param visited
+	 *            the container instances already being stringified
+	 * @return the string representation of the iterator
+	 */
+	private static String ITERATOR_TO_STRING(final Iterator<?> iterator, final IdentityHashMap<Object, Boolean> visited) {
+		final StringBuilder sb = new StringBuilder().append('[');
+		boolean first = true;
+		while (iterator.hasNext()) {
+			if (!first) { sb.append(", "); }
+			sb.append(TO_STRING(iterator.next(), visited));
+			first = false;
+		}
+		return sb.append(']').toString();
+	}
+
+	/**
+	 * A utility method for padding a string with spaces in order to obtain a length of "minLength"
+	 *
+	 * @param string
+	 *            the string to pad
+	 * @param minLength
+	 *            the minimum length to reach (if the string is longer, it will be return as is)
 	 * @return a string of minimum length minLength
 	 */
 	public static String PAD(final String string, final int minLength) {
@@ -68,23 +164,20 @@ public class STRINGS {
 	}
 
 	/**
-	 * A utility method for padding a string with any character in order to obtain a
-	 * length of "minLength"
+	 * A utility method for padding a string with any character in order to obtain a length of "minLength"
 	 *
-	 * @param string    the string to pad
-	 * @param minLength the minimum length to reach (if the string is longer, it
-	 *                  will be return as is)
+	 * @param string
+	 *            the string to pad
+	 * @param minLength
+	 *            the minimum length to reach (if the string is longer, it will be return as is)
 	 * @return a string of minimum length minLength
 	 */
 
 	public static String PAD(final String string, final int minLength, final char c) {
-		if (string.length() >= minLength)
-			return string;
+		if (string.length() >= minLength) return string;
 		final StringBuilder sb = new StringBuilder(minLength);
 		sb.append(string);
-		for (int i = string.length(); i < minLength; i++) {
-			sb.append(c);
-		}
+		for (int i = string.length(); i < minLength; i++) { sb.append(c); }
 		return sb.toString();
 	}
 
