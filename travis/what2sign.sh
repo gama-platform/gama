fc464f605f7eda052f4d64aa2d472cf6fddcdff3
rubenanko <rubenanko@gmail.com>
2026-05-25 17:23:35 +0700

    ci(mac-signing): use bash arrays instead of files in what2sign.sh

--- travis/what2sign.sh
+++ travis/what2sign.sh
@@ -4,28 +4,42 @@
 #	Can automatically parse 4 releases at once
 #
 
-haveLib=false
+# set the list separator to newline
+IFS=$'\n'
+# exit on error
+set -e
 
+# init global variables
+haveLib=false
+NEED_TO_SIGN_FILENAME="needToSign.txt"
+currentAppJar=()
+alreadySawJar=()
+
+# Add a jar to the list of jars to sign
 function addJarInFile(){
-	if [[ $(tail -n 1 needToSign.txt) != "$1" ]]; then
-		echo $1 >> needToSign.txt
+	if [[ $(tail -n 1 $NEED_TO_SIGN_FILENAME) != "$1" ]]; then
+		echo $1 >> $NEED_TO_SIGN_FILENAME
 	fi
 
 	if (( $# != 1 )); then
-		echo "[$1] $2" >> needToSign.txt
+		echo "[$1] $2" >> $NEED_TO_SIGN_FILENAME
 	fi
 }
 
+# List .jar files in the input directory and its subdirectories, without counting global duplicates
 function getJarToCheck(){
-	find "$1" -name "*.jar" > currentAppJar.txt
+	local target_directory="$1"
+
+	# List all the jar files contained in the target directory
+	currentAppJar=($(find "$target_directory" -name "*.jar"))
 
 	# Remove already checked lines
-	grep -v -x -f alreadySawJar.txt currentAppJar.txt > tmp.txt
-	cat tmp.txt > currentAppJar.txt && rm tmp.txt
-
-	cat currentAppJar.txt >> alreadySawJar.txt
+	currentAppJar=($(grep -v -x -f <(printf "%s\n" "${alreadySawJar[@]}") <(printf "%s\n" "${currentAppJar[@]}")))
+	
+	alreadySawJar=("${alreadySawJar[@]}" "${currentAppJar[@]}")
 }
 
+# Check if archive contains *.so, *.dynlib or *.jnilib files
 function haveSomethingToSign(){
 	if [ $(jar tf "$1" | grep '\.so\|\.dylib\|\.jnilib' | wc -l) -gt 0 ]; then
     	# 0 = true
@@ -38,43 +52,46 @@ function haveSomethingToSign(){
 function parseApp(){
 	getJarToCheck "$1"
 
-    while read f
-    do
-		if haveSomethingToSign "$f"; then
-			echo "==> Need to sign $f <=="
-			addJarInFile $f
+    for jarfile in "${currentAppJar[@]}"
+	do
+		if haveSomethingToSign "$jarfile"; then
+			echo "==> Need to sign $jarfile <=="
+			addJarInFile $jarfile
 		else
-			if [ $(jar tf "$f" | grep '\.jar' | wc -l) -gt 0 ]; then
-				jar tf "$f" | grep '\.jar' > nestedJar.txt
-				while read j
+			# Checking the sub .jar files if they exist
+			if [ $(jar tf "$jarfile" | grep '\.jar' | wc -l) -gt 0 ]; then
+				nestedJar=($(jar tf "$jarfile" | grep '\.jar'))
+				for sub_jarfile in "${nestedJar[@]}"
 				do
-					echo "[$(echo $f | rev | cut -d "/" -f 1 | rev)] Check in $j"
-					jar xf "$f" "$j"
-					if haveSomethingToSign "$j"; then
-						echo "==> Need to sign $j <=="
-						addJarInFile "$f" "$j"
+					echo "[$(echo $jarfile | rev | cut -d "/" -f 1 | rev)] Check in $sub_jarfile"
+					jar xf "$jarfile" "$sub_jarfile"
+					if haveSomethingToSign "$sub_jarfile"; then
+						echo "==> Need to sign $sub_jarfile <=="
+						addJarInFile "$jarfile" "$sub_jarfile"
 					fi
-				done < nestedJar.txt
+				done
 			fi
 		fi 
-
-    done < currentAppJar.txt
+    done
 }
 
 function unzipAndParse(){
-	echo "Unzipping $1 ..."
-	unzip -q "$1"
+	local filename="$1"
+
+	echo "Unzipping $filename ..."
+	unzip -q "$filename"
 	parseApp "./Gama.app"
+	
+	# Clean-up the created directories
 	find . -mindepth 1 -maxdepth 1 -type d -exec rm -fr {} \;
 	echo "xxx"
 }
 
-
-touch alreadySawJar.txt needToSign.txt currentAppJar.txt nestedJar.txt
+touch $NEED_TO_SIGN_FILENAME
 for gama in ./gama.application-macosx*.zip; do
 	unzipAndParse "$gama"
 done
 
 # Remove duplicated lines
-awk '!a[$0]++' needToSign.txt > tmp.txt
-cat tmp.txt > needToSign.txt
+tmp=($(awk '!a[$0]++' $NEED_TO_SIGN_FILENAME))
+printf "%s\n" ${tmp[@]} > $NEED_TO_SIGN_FILENAME
