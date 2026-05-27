2bd6cd03d46a83618829cf3ec97678123abee353
rubenanko <rubenanko@gmail.com>
2026-05-27 10:11:00 +0700

    feat(): maven-build-cache extension configuration

--- travis/build.sh
+++ travis/build.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Compiling gama.annotations"
 cd $( dirname $( realpath "${BASH_SOURCE[0]}" ) )/../gama.annotations
-mvn clean install "$@"
+mvn clean install -Dmaven.build.cache.configPath=maven-build-cache-config.xml "$@"
 
 echo "Compiling gama.processor"
 cd ../gama.processor 
-mvn clean install "$@"
+mvn clean install -Dmaven.build.cache.configPath=maven-build-cache-config.xml "$@"
 
 echo "Compiling gama.parent"
 cd ../gama.parent 
-mvn clean install "$@"
+mvn clean install -Dmaven.build.cache.configPath=maven-build-cache-config.xml "$@"
