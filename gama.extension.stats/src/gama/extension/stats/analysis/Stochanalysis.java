ee0271e46843ce6e4051dbbca513eefdfbf62d13
Paul Saves <paul.saves@alumni.enac.fr>
2026-07-12 17:56:52 -0400

    Fix: missing import java.util.Optional

--- gama.extension.stats/src/gama/extension/stats/analysis/Stochanalysis.java
+++ gama.extension.stats/src/gama/extension/stats/analysis/Stochanalysis.java
@@ -22,6 +22,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
