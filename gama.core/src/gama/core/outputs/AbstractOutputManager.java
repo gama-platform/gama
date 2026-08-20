751905f08b0660657cf3129f93654c728dc0acfc
AlexisDrogoul <579256+AlexisDrogoul@users.noreply.github.com>
2026-08-20 05:37:15 +0000

    Fix synchronized output affecting other experiments (#1158)
    - Remove side-effect calls to GAMA.synchronizeFrontmostExperiment() in AbstractOutputManager constructor and setChildren, which were executed during compilation and model parsing.
    - Add isSynchronized() to IOutputManager and AbstractOutputManager to query synchronization requirements dynamically.
    - Synchronize frontmost experiment in AbstractOutputManager.init(scope) when running an experiment.
    - Reset synchronization state in ExperimentAgent.init(scope) upon experiment initialization.

--- gama.core/src/gama/core/outputs/AbstractOutputManager.java
+++ gama.core/src/gama/core/outputs/AbstractOutputManager.java
@@ -299,9 +299,6 @@ public abstract class AbstractOutputManager extends Symbol implements IOutputMan
 	public AbstractOutputManager(final IDescription desc) {
 		super(desc);
 		autosave = desc.getFacetExpr(IKeyword.AUTOSAVE);
-		boolean sync = GamaPreferences.Runtime.CORE_SYNC.getValue() || "true".equals(desc.getLitteral("synchronized"))
-				|| desc.hasFacet(IKeyword.AUTOSAVE) && !"false".equals(desc.getLitteral(IKeyword.AUTOSAVE));
-		if (sync) { GAMA.synchronizeFrontmostExperiment(); }
 	}
 
 	// @Override
@@ -380,7 +377,6 @@ public abstract class AbstractOutputManager extends Symbol implements IOutputMan
 			if (s instanceof LayoutStatement) {
 				layout = (LayoutStatement) s;
 			} else if (s instanceof IOutput o) {
-				if (o.isAutoSave()) { GAMA.synchronizeFrontmostExperiment(); }
 				add(o);
 				o.setUserCreated(false);
 				if (o instanceof LayeredDisplayOutput ldo) { ldo.setIndex(displayIndex++); }
@@ -419,7 +415,22 @@ public abstract class AbstractOutputManager extends Symbol implements IOutputMan
 	}
 
 	@Override
+	public boolean isSynchronized() {
+		if (GamaPreferences.Runtime.CORE_SYNC.getValue()) return true;
+		final IDescription desc = getDescription();
+		if (desc != null) {
+			if ("true".equals(desc.getLitteral("synchronized"))) return true;
+			if (desc.hasFacet(IKeyword.AUTOSAVE) && !"false".equals(desc.getLitteral(IKeyword.AUTOSAVE))) return true;
+		}
+		for (final IOutput o : outputs.values()) {
+			if (o.isAutoSave()) return true;
+		}
+		return false;
+	}
+
+	@Override
 	public boolean init(final IScope scope) {
+		if (isSynchronized()) { GAMA.synchronizeFrontmostExperiment(); }
 		name = scope.getRoot().getName();
 		for (final IOutput output : ImmutableList.copyOf(outputs.values())) { if (!open(scope, output)) return false; }
 		evaluateAutoSave(scope);
