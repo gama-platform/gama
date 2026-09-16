f1a547ebbb736363e9cd1538fb4714d2b2896600
AlexisDrogoul <579256+AlexisDrogoul@users.noreply.github.com>
2026-09-16 02:10:54 +0000

    fix: update Java version check in gama-headless.sh for Java 25
    The headless runner script gama-headless.sh previously had an outdated
    and broken Java version check expecting Java 21. Since GAMA 2026 and
    the CI workflows use Java 25, this caused the CI 'Test release' step
    in travis-build-test.yaml to fail when running gama-headless.sh.
    
    This updates the script to properly check for Java 25 or higher.

--- gama.product/extraresources/headless/unix/gama-headless.sh
+++ gama.product/extraresources/headless/unix/gama-headless.sh
@@ -19,9 +19,13 @@ if [ -d "${headlessPath}/../jdk" ]; then
     java+="bin/java"
 else
   javaVersion=$(java -version 2>&1 | head -n 1 | cut -d "\"" -f 2)
-  # Check if good Java version before everything
-  if [[ ${javaVersion:2} == 21 ]]; then
-    echo "You should use Java 21 to run GAMA"
+  majorVersion=$(echo "$javaVersion" | cut -d '.' -f 1)
+  if [[ "$majorVersion" == "1" ]]; then
+    majorVersion=$(echo "$javaVersion" | cut -d '.' -f 2)
+  fi
+  # Check if Java version is at least 25
+  if [[ "$majorVersion" -lt 25 ]]; then
+    echo "You should use Java 25 to run GAMA"
     echo "Found you using version : $javaVersion"
     exit 1
   fi
